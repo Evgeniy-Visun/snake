362a6a002168a998343f496abca03c6ae5a0a15a
Visun <visune.v@yandex.ru>
2016-05-30 19:44:40 +0300

    Add classes (horisontal and vertical)

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char symb)
+        {
+            pList = new List<Point>();
+
+            //Point p1 = new Point(5, 4, '*');
+            //Point p2 = new Point(6, 4, '*');
+            //Point p3 = new Point(7, 4, '*');
+
+            //pList.Add(p1);
+            //pList.Add(p2);
+            //pList.Add(p3);
+
+            for (int x=xLeft; x<=xRight; x++)
+            {
+                Point p = new Point(x, y, symb);
+                pList.Add(p);
+            }
+            
+        }
+
+        public void DrawHorisontal()
+        {
+            foreach (Point p in pList)
+                p.Draw();
+            }
+        }
+    }
+
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine (int yUp, int yDown, int x, char symb)
+        {
+            pList = new List<Point>();
+
+            for (int y = yUp; y<=yDown; y++)
+            {
+                Point p = new Point(x, y, symb);
+                pList.Add(p);
+            }
+        }
+
+        public void DrawVertical()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
